1275b098c4790857989bddc598e5bc179b9073ec
samdmarshall <me@samdmarshall.com>
2014-05-08 10:21:45 -0400

    xpc/private.h and filevault

---
+++ usr/include/xpc/private.h
@@ -0,0 +1,6 @@
+#include <xpc/xpc.h>
+
+extern xpc_object_t xpc_connection_copy_entitlement_value(xpc_connection_t connection, const char *entitlement);
+
+extern void xpc_connection_set_target_uid(xpc_connection_t connection, uid_t uid);
+
