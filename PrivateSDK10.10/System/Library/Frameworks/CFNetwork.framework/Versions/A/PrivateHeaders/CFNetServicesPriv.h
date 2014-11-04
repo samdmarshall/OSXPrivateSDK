563ac679617f48fcb0897e41a4ab3968dc9fe733
samdmarshall <me@samdmarshall.com>
2014-11-04 14:17:58 -0800

    objc, ccl, openpam, xar, zlib, openal, servicemanagement, libtop, libsntp

--- PrivateSDK10.10/System/Library/Frameworks/CFNetwork.framework/Versions/A/PrivateHeaders/CFNetServicesPriv.h
+++ PrivateSDK10.10/System/Library/Frameworks/CFNetwork.framework/Versions/A/PrivateHeaders/CFNetServicesPriv.h
@@ -1,8 +1,12 @@
 #ifndef __CFNETSERVICESPRIV_H__
 #define __CFNETSERVICESPRIV_H__
 
-#include <CoreFoundation/CoreFoundation.h>
+#include <CFNetwork/CFNetwork.h>
 
 extern CFNetServiceRef _CFNetServiceCreateFromURL(CFAllocatorRef allocator, CFURLRef url);
+
+const SInt32 kCFStreamErrorHTTPConnectionLost = -4;
+
+extern CFStringRef _kCFStreamSSLTrustedLeafCertificates;
 
 #endif
--- PrivateSDK10.10/usr/local/include/os/lock_private.h
+++ PrivateSDK10.10/usr/local/include/os/lock_private.h
@@ -6,11 +6,11 @@
 #define OS_LOCK_HANDOFF_INIT 0
 #define OS_LOCK_SPIN_INIT 0
 
-typedef int32_t os_lock_handoff_s;
-typedef int32_t os_lock_spin_s;
+typedef int32_t * os_lock_handoff_s;
+typedef int32_t * os_lock_spin_s;
 
-extern bool os_lock_trylock(os_lock_spin_s lock);
-extern void os_lock_lock(os_lock_spin_s lock);
-extern void os_lock_unlock(os_lock_spin_s lock);
+extern bool os_lock_trylock(int32_t * lock);
+extern void os_lock_lock(int32_t * lock);
+extern void os_lock_unlock(int32_t * lock);
 
 #endif
