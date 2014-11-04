772af277dab0a4d1939bea06bda58b0d720114c8
samdmarshall <me@samdmarshall.com>
2014-11-04 14:38:41 -0800

    fixes for lock_private and getting objc to build with order file

--- PrivateSDK10.10/usr/local/include/os/lock_private.h
+++ PrivateSDK10.10/usr/local/include/os/lock_private.h
@@ -6,11 +6,11 @@
 #define OS_LOCK_HANDOFF_INIT 0
 #define OS_LOCK_SPIN_INIT 0
 
-typedef int32_t * os_lock_handoff_s;
-typedef int32_t * os_lock_spin_s;
+typedef uintptr_t os_lock_handoff_s;
+typedef uintptr_t os_lock_spin_s;
 
-extern bool os_lock_trylock(int32_t * lock);
-extern void os_lock_lock(int32_t * lock);
-extern void os_lock_unlock(int32_t * lock);
+extern bool os_lock_trylock(uintptr_t lock);
+extern uintptr_t os_lock_lock(uintptr_t lock);
+extern uintptr_t os_lock_unlock(uintptr_t lock);
 
 #endif
