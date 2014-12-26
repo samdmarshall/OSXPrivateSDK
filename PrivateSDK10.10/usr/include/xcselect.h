5ecedbce7792041aa031fd243364c8ced10d0018
samdmarshall <me@samdmarshall.com>
2014-12-26 12:58:56 -0500

    updating xcselect header

--- PrivateSDK10.10/usr/include/xcselect.h
+++ PrivateSDK10.10/usr/include/xcselect.h
@@ -13,5 +13,7 @@ uint32_t xcselect_manpaths_get_num_paths(xcselect_manpaths *xcp);
 const char * xcselect_manpaths_get_path(xcselect_manpaths *xcp, unsigned index);
 void xcselect_manpaths_append(xcselect_manpaths *xcp, char *path);
 void xcselect_manpaths_free(xcselect_manpaths *xcp);
-	
+bool xcselect_trigger_install_request(char *name);
+void xcselect_invoke_xcrun(char *tool_name, int argc, char *argv[], int zero);
+
 #endif
