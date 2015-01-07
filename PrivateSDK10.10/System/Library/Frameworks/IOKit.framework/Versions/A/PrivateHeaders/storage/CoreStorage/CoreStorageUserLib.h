e5476e4a213463314b8754615aa6f092b8cfeb7f
samdmarshall <me@samdmarshall.com>
2015-01-07 12:40:58 -0500

    adding IOKit headers

--- PrivateSDK10.10/System/Library/Frameworks/IOKit.framework/Versions/A/PrivateHeaders/storage/CoreStorage/CoreStorageUserLib.h
+++ PrivateSDK10.10/System/Library/Frameworks/IOKit.framework/Versions/A/PrivateHeaders/storage/CoreStorage/CoreStorageUserLib.h
@@ -7,6 +7,19 @@
 #define kCoreStorageNotificationLVGChanged "CoreStorageNotificationLVGChanged"
 #define kCoreStorageNotificationVolumeChanged "CoreStorageNotificationVolumeChanged"
 
+#define kCoreStorageGroupTypeKey "com.apple.corestorage.lvg.type"
+#define kCoreStorageLogicalGroupUUIDKey "com.apple.corestorage.lv.groupUUID"
+#define kCoreStorageGroupTypeCPDK "CPDK"
+#define kCoreStorageLogicalClassName "CoreStorageLogical"
+
+#define kCSFDEEFILoginUnlockIdentID "efilogin-unlock-ident"
+
 kern_return_t CoreStorageEnableNotifications();
+
+typedef CFStringRef CoreStorageLogicalRef;
+typedef CFStringRef CoreStorageGroupRef;
+
+CFMutableDictionaryRef CoreStorageCopyVolumeProperties(CoreStorageLogicalRef lvgUUID);
+CFMutableDictionaryRef CoreStorageCopyLVGProperties(CoreStorageLogicalRef lvgUUID);
 
 #endif
