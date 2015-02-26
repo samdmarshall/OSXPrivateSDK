69bf3c7f7140ed6ab2b6684b427bd457209858fe
samdmarshall <me@samdmarshall.com>
2015-02-26 17:26:11 -0500

    adding libsecurity libraries and additional headers

---
+++ PrivateSDK10.10/System/Library/Frameworks/CFNetwork.framework/Versions/A/PrivateHeaders/CFSocketStreamPriv.h
@@ -0,0 +1,8 @@
+#ifndef __CFSOCKETSTREAMPRIV_H__
+#define __CFSOCKETSTREAMPRIV_H__
+
+#include <CFNetwork/CFNetwork.h>
+
+extern CFStringRef kCFStreamPropertyBoundInterfaceIdentifier;
+
+#endif
---
+++ PrivateSDK10.10/usr/include/ne_sm_bridge.h
@@ -0,0 +1,9 @@
+#ifndef __NE_SM_BRIDGE_H__
+#define __NE_SM_BRIDGE_H__
+
+typedef struct ne_sm_bridge_t *ne_sm_bridge_t;
+
+#define NESessionStartOptionOutgoingInterface ""
+
+
+#endif
